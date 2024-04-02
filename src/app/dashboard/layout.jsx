550c8a5fbff87bfa02f479ac61c583558c644bdc
Thiem Huynh <thiem.huynh0@gmail.com>
2024-04-02 17:53:42 -0400

    setup pages

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,13 +1,14 @@
 import NavBar from '../ui/dashboard/navbar/navbar';
 import SideBar from '../ui/dashboard/sidebar/sidebar';
+import styles from '../ui/dashboard/dashboard.module.css';
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.menu}>
         <SideBar />
       </div>
-      <div>
+      <div className={styles.content}>
         <NavBar />
         {children}
       </div>
--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,100 @@
+import MenuLink from './menuLink/menuLink';
+import Image from 'next/image';
+import styles from './sidebar.module.css';
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from 'react-icons/md';
+
+const menuItems = [
+  {
+    title: 'Pages',
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboard />,
+      },
+      {
+        title: 'Users',
+        path: '/dashboard/users',
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: 'Transactions',
+        path: '/dashboard/transactions',
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    list: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        icon: <MdWork />,
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <MdAnalytics />,
+      },
+      {
+        title: 'Teams',
+        path: '/dashboard/teams',
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: 'User',
+    list: [
+      {
+        title: 'Settings',
+        path: '/dashboard/settings',
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: 'Help',
+        path: '/dashboard/help',
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
 const SideBar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image src= alt='user profile' width='50' height='50'/>
+      </div>
+      <ul>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
