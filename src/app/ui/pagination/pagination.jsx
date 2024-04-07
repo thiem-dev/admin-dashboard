f6b22a3df4f77725075a50e0d8082ab9442ee095
Thiem Huynh <thiem.huynh0@gmail.com>
2024-04-07 09:53:18 -0400

    pagination code n button setup

--- src/app/ui/pagination/pagination.jsx
+++ src/app/ui/pagination/pagination.jsx
@@ -1,6 +1,29 @@
+'use client';
 import styles from './pagination.module.css';
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  // const page = searchParams.get("page") || 1;
+  const handleChangePage = (type) => {
+    console.log(type);
+  };
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        // disabled={!hasPrev}
+        onClick={() => handleChangePage('prev')}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+        // disabled={!hasNext}
+        onClick={() => handleChangePage('next')}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 export default Pagination;
